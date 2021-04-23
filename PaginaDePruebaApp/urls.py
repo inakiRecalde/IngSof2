d9402c51d7d3697121f07136718e07165b01c523
MechiScattini <mechiscatt@gmail.com>
2021-04-23 15:04:19 -0300

    url admin

--- PaginaDePruebaApp/urls.py
+++ PaginaDePruebaApp/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path
 from PaginaDePruebaApp import views
+from django.contrib import admin
 
 urlpatterns = [
+    path('admin',admin.site.urls),
     path('Inicio', views.Inicio,name = "Inicio"),
     path('Comentarios', views.Comentarios,name = "Comentarios"),
     path('Perfil', views.Perfil,name = "Perfil"),
     path('Contacto', views.Contacto,name = "Contacto"),
-
+    
 ]
