374fea8c501cb6b38bad700b3e2452ac0e23ddbe
inakiRecalde <iniakirecalde@hotmail.com>
2021-04-23 03:47:31 -0300

    Probando panel de administracion admin: adminCombi19 contra: 12345

--- PaginaDePruebaApp/admin.py
+++ PaginaDePruebaApp/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
 
+from PaginaDePruebaApp.models import *
+
 # Register your models here.
+
+##UserAdmin  adminCombi19 contra: 12345
+
+class UsuarioAdmin(admin.ModelAdmin):
+    list_display = ("nombre", "email","dni")  #campos de la tabla
+    search_fields = ("nombre","apellido")  ## campos por los que se puede buscar
+
+    #list_filter("") muestra filtros tener en cuaenta para los viajes para filtrar por fechas
+admin.site.register(Usuario, UsuarioAdmin)
+
+admin.site.register(Combi)
+
+admin.site.register(Insumo)
+
+admin.site.register(Chofer)
--- PaginaDePruebaApp/models.py
+++ PaginaDePruebaApp/models.py
@@ -1,3 +1,43 @@
 from django.db import models
+from django.contrib.auth.admin import User
 
 # Create your models here.
+
+class Usuario(models.Model):
+    nombre=models.CharField(max_length=30)
+    apellido=models.CharField(max_length=30)
+    email= models.EmailField()
+    dni= models.IntegerField()
+    contraseña = models.CharField(max_length=50)
+
+    def __str__(self):
+        return (self.nombre)
+
+
+
+class Chofer(models.Model):
+    nombre=models.CharField(max_length=30)
+    apellido=models.CharField(max_length=30)
+    email= models.EmailField()
+    telefono= models.IntegerField()
+    contraseña = models.CharField(max_length=50)
+
+    def __str__(self):
+        return (self.nombre)
+
+
+class Combi(models.Model):
+    marca=models.CharField(max_length=30)
+    modelo=models.CharField(max_length=30)
+    cantAsientos= models.IntegerField()
+    patente = models.CharField(max_length=20)
+    chofer = models.OneToOneField(Chofer, on_delete= models.CASCADE, ## cascade mantiene la integridad referencial ante bajas
+                                                null= False, blank = False) ## obliga a que se asigne un chofer
+
+
+class Insumo(models.Model):
+    nombre=models.CharField(max_length=30)
+    precio=models.IntegerField()
+
+    def __str__(self):
+        return 'el insumo %s tiene un precio de $ %s  ' %(self.nombre, self.precio) ## asi se muestra en la tabla
